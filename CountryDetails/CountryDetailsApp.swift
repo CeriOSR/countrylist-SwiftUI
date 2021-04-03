4cf384ae70b0af293350335908bac252d5c363bb
Rey Cerio <rey.cerio@appetiser.com.au>
2021-04-03 12:14:32 -0700

    Initial Commit

---
+++ CountryDetails/CountryDetailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryDetailsApp.swift
+//  CountryDetails
+//
+//  Created by Rey Cerio on 2021-04-03.
+//
+
+import SwiftUI
+
+@main
+struct CountryDetailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
